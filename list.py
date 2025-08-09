3e3d8505a796b1b15a8f0875281286262e22d85c
Hope Nyvah <nyvahhope@gmail.com>
2025-08-09 13:21:01 +0300

    python list

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list = []
+numbers = [10, 20, 30, 40]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1,15)
+print(my_list)
+my_list.extend([50,60,70])
+print(my_list)
+del my_list[7]
+print(my_list)
+my_list.sort()
+print(my_list)
+print("Index of 30:", my_list.index(30))
+
